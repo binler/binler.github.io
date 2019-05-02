a0c1583e225db5ee4981c7cc615d4f84c1bf80b0
GLODEIT\Binh.LT <binh.lt@globaldesignit.vn>
2019-05-02 16:17:20 +0700

    Update content site

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,3 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
@@ -38,7 +31,7 @@ const Layout = ({ children }) => (
           <footer>
             © {new Date().getFullYear()}, Built with
             {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
+            <a href="https://binler.github.io/">binlerdev</a>
           </footer>
         </div>
       </>
