e84569a13215e13722b161c98e049d948af11289
Binh. Le Tat <binh.lt@globaldesignit.vn>
2020-07-08 10:36:36 +0700

    Update theme for porfolio

---
+++ gatsby-theme-intro/src/types/social.js
@@ -0,0 +1,26 @@
+import { graphql } from "gatsby"
+import { string, oneOf } from "prop-types"
+
+export const SocialType = {
+  name: oneOf([
+    "behance",
+    "dribbble",
+    "facebook",
+    "github",
+    "goodreads",
+    "instagram",
+    "linkedin",
+    "producthunt",
+    "twitter",
+    "youtube",
+    "xing",
+  ]),
+  url: string,
+}
+
+export const query = graphql`
+  fragment SocialFragment on SocialYaml {
+    name
+    url
+  }
+`
